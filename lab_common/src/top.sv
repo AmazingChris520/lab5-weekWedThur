a4c24ed1e9976a4dfd551be760c1df74dcd5d2cd
sudoNeo <mlope589@ucr.edu>
2026-05-05 16:45:49 -0700

    all white

--- lab_common/src/top.sv
+++ lab_common/src/top.sv
@@ -1,54 +1,46 @@
-module top
-(
-    input  CLK,            // 25 MHz from P6
-    output LCD_CLK,        
-    output LCD_DEN,        
-    output [4:0] LCD_R,
-    output [5:0] LCD_G,
-    output [4:0] LCD_B
+module top (
+    input  wire CLK,
+
+    output wire LCD_CLK,
+    output wire LCD_DEN,
+
+    output wire [4:0] LCD_R,
+    output wire [5:0] LCD_G,
+    output wire [4:0] LCD_B
 );
+
+    localparam H_ACTIVE = 480;
+    localparam H_TOTAL  = 525;
+
+    localparam V_ACTIVE = 272;
+    localparam V_TOTAL  = 285;
+
+    reg [9:0] x = 10'd0;
+    reg [8:0] y = 9'd0;
 
     assign LCD_CLK = CLK;
 
-    // timing
-    localparam H_ACTIVE = 10'd480;
-    localparam H_TOTAL  = 10'd525;
-    localparam V_ACTIVE = 10'd272;
-    localparam V_TOTAL  = 10'd285;
-
-    
-    reg [9:0] x = 0; // 0-524
-    reg [9:0] y = 0;// 0-284
-
     always @(posedge CLK) begin
         if (x == H_TOTAL - 1) begin
-            x <= 0;
-            y <= (y == V_TOTAL - 1) ? 10'd0 : y + 1'b1;
+            x <= 10'd0;
+
+            if (y == V_TOTAL - 1)
+                y <= 9'd0;
+            else
+                y <= y + 9'd1;
+
         end else begin
-            x <= x + 1'b1;
+            x <= x + 10'd1;
         end
     end
 
+    wire active;
+    assign active = (x < H_ACTIVE) && (y < V_ACTIVE);
 
-    wire active = (x < H_ACTIVE) && (y < V_ACTIVE);
     assign LCD_DEN = active;
 
+    assign LCD_R = active ? 5'b11111  : 5'b00000;
+    assign LCD_G = active ? 6'b111111 : 6'b000000;
+    assign LCD_B = active ? 5'b11111  : 5'b00000;
 
-    reg [4:0] r;
-    reg [5:0] g;
-    reg [4:0] b;
-
-    always @(*) begin
-        r = 5'd0;
-        g = 6'd0;
-        b = 5'd0;
-        if (active) begin
-            if (x < 10'd160) r = 5'd31;   
-            else if (x < 10'd320) g = 6'd63;  
-            else b = 5'd31;  
-        end
-    end
-    assign LCD_R = r;
-    assign LCD_G = g;
-    assign LCD_B = b;
 endmodule
