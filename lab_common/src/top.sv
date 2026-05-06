b7887e1ccdf4536c60dc0f7d815edc73ae74faa4
sudoNeo <mlope589@ucr.edu>
2026-05-05 18:51:17 -0700

    Added display code

--- lab_common/src/top.sv
+++ lab_common/src/top.sv
@@ -1,46 +1,56 @@
 module top (
-    input  wire CLK,
-
-    output wire LCD_CLK,
-    output wire LCD_DEN,
-
-    output wire [4:0] LCD_R,
-    output wire [5:0] LCD_G,
-    output wire [4:0] LCD_B
+    input   CLK,
+    output  LCD_CLK,
+    output  LCD_DEN,
+    output  [4:0] LCD_R,
+    output  [5:0] LCD_G,
+    output  [4:0] LCD_B
 );
-
     localparam H_ACTIVE = 480;
     localparam H_TOTAL  = 525;
-
     localparam V_ACTIVE = 272;
     localparam V_TOTAL  = 285;
 
-    reg [9:0] x = 10'd0;
-    reg [8:0] y = 9'd0;
+    reg [9:0] x = 0;
+    reg [8:0] y = 0;
 
     assign LCD_CLK = CLK;
 
     always @(posedge CLK) begin
-        if (x == H_TOTAL - 1) begin
-            x <= 10'd0;
-
-            if (y == V_TOTAL - 1)
-                y <= 9'd0;
-            else
-                y <= y + 9'd1;
-
+        if (x < H_TOTAL - 1) begin
+            x <= x +1;
         end else begin
-            x <= x + 10'd1;
+            x <=0;
+            if (y < V_TOTAL -1 ) begin
+            y<= y+1;
+            end else begin 
+            y<=0;
+            end
         end
     end
 
-    wire active;
-    assign active = (x < H_ACTIVE) && (y < V_ACTIVE);
-
-    assign LCD_DEN = active;
-
-    assign LCD_R = active ? 5'b11111  : 5'b00000;
-    assign LCD_G = active ? 6'b111111 : 6'b000000;
-    assign LCD_B = active ? 5'b11111  : 5'b00000;
-
+    
+    assign LCD_DEN = (x < H_ACTIVE) && (y < V_ACTIVE);
+    always@(*) begin
+        if (LCD_DEN) begin
+            if (x<160) begin
+                LCD_R = 5'd31;
+                LCD_G = 6'd0;
+                LCD_B = 6'd0;
+            end else if (x<320) begin 
+                LCD_R = 5'd0;
+                LCD_G = 6'd62;
+                LCD_B = 6'd0;
+            end else begin 
+                LCD_R = 5'd0;
+                LCD_G = 6'd0;
+                LCD_B = 6'd31;
+            end
+        end else begin 
+            LCD_R = 5'd0;
+            LCD_G = 6'd0;
+            LCD_B = 6'd0;
+        end
+    end
+              
 endmodule
